fc4989660e837a561db8e0a9b07e706ca9c6f3f7
danv <dannyvera@gmail.com>
2020-07-14 13:16:03 -0400

    removed event listeners after a user has made a choise. Cleaned up code and fixed other bugs

--- game.js
+++ game.js
@@ -1,24 +1,35 @@
-//global object variable for character list with image url's
+//global array for character choices on screen
 const choiceArr = new Array(4);
+
+//global variable for score
 let score = 0;
-//generates an object with the name:imageURL as key value pairs 
+
+//downloads all characters and returns an array 
 const charImageList = async () => {
   try {
     const URL_ALL = `https://api.got.show/api/show/characters/`;
     const allCharObj = await axios.get(URL_ALL);
+
+    // for testing. Logs all the records and if they load their images correctly
     // for (let i = 0; i < allCharObj.data.length; i++) {
     //   let image = document.createElement('img')
     //   image.src = allCharObj.data[i].image;
-    //   console.log(i, allCharObj.data[i].name);
+    //   //console.log(i, allCharObj.data[i].name);
     //   image.onload = function () {
-
     //     console.log(i, 'image loaded');
     //   }
     //   image.onerror = function (error) {
-    //     console.error(`this image didnt load`, i)
+    //     console.error(`this image didnt load`, i, allCharObj.data[i].image, error)
     //   }
     // }
     // console.log(allCharObj.data);
+
+    // removing records that I know have problems loading the images.
+    allCharObj.data.splice(146, 1);
+    allCharObj.data.splice(97, 1);
+    allCharObj.data.splice(92, 1);
+    allCharObj.data.splice(90, 1);
+    console.log(allCharObj.data);
     return allCharObj.data;
   } catch (error) {
     console.error(`you have an error${error}`)
@@ -39,38 +50,86 @@ const randomQuote = async () => {
 // checks correct answer and displays the appropriate text
 const answer = (data, correct) => {
   let img = document.querySelectorAll('img');
-  let x;       //correct image index
+  let x;       //correct image index stored in x
   for (let i = 0; i < img.length; i++) {
-    // console.log(data.image === img[i].src);
-    //console.log(data.image);
+
     if (data.image == img[i].src) {
       img[i].classList.add("largeImg");
-      let parentAside = img[i].parentElement;
-      parentAside.classList.add('width-auto');
-      if (parentAside.id === 'imgs1') {
-        document.querySelector('#imgs2').classList.add("width-0");
-      } else {
-        document.querySelector('#imgs1').classList.add("width-0");
-      }
-      x = i;
+      x = i;        // storing the correct answer image index
     } else {
       img[i].classList.add("smallImg");
       img[i].style.opacity = "0";
-      setTimeout(() => { img[i].style.display = 'none'; }, 200);
+      setTimeout(() => { img[i].style.display = 'none'; }, 300);
     }
   }
-  console.log("correct answer?", correct);
-  if (correct) {
-    document.querySelector('p').innerHTML = `CORRECT!!<br>It was <b>${data.name}</b> sffsdf sdfdsfsd sdf sdfsd fsdfdsfsd fdsfsdfsd fsdfdsf sf sdf ds fsdfsdfsdfsdfsdf s sdf`
-    score += 1;
-    document.querySelector('#score').innerText = score;
-    //console.log(img[x].classList);
-  } else {
-    document.querySelector('p').innerHTML = `INCORRECT!!<br>It was <b>${data.name}</b> sffsdf sdfdsfsd sdf sdfsd fsdfdsfsd fdsfsdfsd fsdfdsf sf sdf ds fsdfsdfsdfsdfsdf s sdf`
-    score -= 1;
-    document.querySelector('#score').innerText = score;
-    //console.log(img[x].classList);
+  // increasing the width of the parent of the correct answer image
+  let parentAside = img[x].parentElement;
+  let asides = document.querySelectorAll('aside');
+  for (let i = 0; i < asides.length; i++) {
+    if (asides[i] === parentAside) {
+      asides[i].classList.add("width-auto");
+    } else {
+      asides[i].classList.add("width-0");
+    }
   }
+  // cloning the node to remove the event listener on the large image. Learned this technique from "BenD"
+  // https://stackoverflow.com/questions/9251837/how-to-remove-all-listeners-in-an-element
+  let newImg = img[x].cloneNode();
+  setTimeout(() => { parentAside.replaceChild(newImg, img[x]); }, 300); //settimeout to allow for animations
+  //console.log("correct answer?", correct);
+
+  // text to display if correct or incorrect
+  console.log('constructing answer', data)
+  let descText;
+  let isCorrect;
+  let pronoun;
+  let tense;
+  let allTitles;
+  let pluralTitles;
+  let alive;
+  if (correct) {
+    isCorrect = 'CORRECT!!!'
+  } else {
+    isCorrect = 'INCORRECT!!!'
+  }
+
+  if (data.gender === 'male') {
+    pronoun = 'His'
+  } else {
+    pronoun = 'Her'
+  }
+
+  if (data.alive == true) {
+    tense = 'is';
+    alive = '';
+  } else {
+    tense = 'was';
+    alive = " And is no longer with us.";
+  }
+
+
+  if (data.titles.length = 1) {
+    pluralTitles = 'title includes';
+    allTitles = data.titles;
+  } else {
+    pluralTitles = 'titles include';
+    allTitles = data.titles[0]
+    for (let i = 1; i < data.titles.length; i++) {
+      if (data.titles[i] !== data.titles.length - 1) {
+        allTitles.concat(", ", data.titles[i])
+      } else {
+        allTitles.concat(" and ", data.titles[i])
+      }
+    }
+  }
+
+
+
+  descText = `${pronoun} ${pluralTitles} ${allTitles}`
+
+  document.querySelector('p').innerHTML = `${isCorrect}<br>It ${tense} <b>${data.name}</b> of ${data.house}. ${descText}.${alive}`
+  score += 1;
+  document.querySelector('#score').innerText = score;
 }
 
 const rand = (max) => {
@@ -82,14 +141,14 @@ const choices = async (name) => {
   try {
     const charURL = `https://api.got.show/api/show/characters/${name}`;
     const response = await axios.get(charURL);
-    //console.log(response, `this is ther name ${name}`);
-    //console.log(choiceArr);
+    console.log(response);
     let img = document.querySelectorAll('img');
     // setting the image index of the correct answer
     let ansLoc = rand(img.length);
     img[ansLoc].src = response.data.image;
     // calling the fuction to update score and display the appropriate text when correct
     img[ansLoc].addEventListener('click', () => {
+
       answer(response.data, true);
     });
 
@@ -115,11 +174,9 @@ const choices = async (name) => {
           }
         }
         // only fills in the global array if not a duplicate. 
-        //Getting one step closer to exiting the loop
-        console.log("it contains an image", ('image' in allChar[x]));
-        // x cannot equal 146 or 97 because the image links are broken
+        // Which gets you one step closer to exiting the loop
         // also checking for missing image keys and duplicates
-        if (isDup !== true && ('image' in allChar[x]) && !(allChar[x].image == '') && (x != 146 || x != 97)) {
+        if (isDup !== true && ('image' in allChar[x]) && !(allChar[x].image == '')) {
           image.src = allChar[x].image;
           choiceArr[i] = allChar[x];
           // calling the fuction to update score and display the appropriate text when wrong
@@ -131,7 +188,6 @@ const choices = async (name) => {
         }
       }
     }
-    //console.log(choiceArr)
     for (let i = 0; i < img.length; i++) {
       img[i].style.opacity = "1.0";
     }
@@ -140,8 +196,6 @@ const choices = async (name) => {
     console.error(`Hey you got an error${error}`)
   }
 }
-
-
 
 // checks and fixes names that dont match correctly 
 // from the quotes database to the character data
@@ -163,7 +217,7 @@ const checkChar = (data) => {
 
   choices(name);
 }
-
+// displays the random quote
 const displayQuote = (data) => {
   document.querySelector('#quote').innerText = data.sentence;
   document.querySelector('#quote').style.opacity = "1.0";
@@ -176,15 +230,17 @@ const nextQuote = async () => {
     document.querySelector('#imgs1').classList.remove('width-auto', 'width-0');
     document.querySelector('#imgs2').classList.remove('width-auto', 'width-0');
     // have to delete the img elemtents and recreate them to clear all the eventListeners. 
-    // Trust me this is the fastest way
+    // Trust me this is the fastest way Learned this technique from "BenD"
+    // https://stackoverflow.com/questions/9251837/how-to-remove-all-listeners-in-an-element
     let newImg = document.createElement('img');
     newImg.style.opacity = "0";
     img[i].style.opacity = "0";
     img[i].parentElement.append(newImg);
     img[i].remove();
-    // opacity is 0 at this point so making the display visible is OK
     newImg.style.display = "block";
-    console.log(newImg);
+    // opacity is 0 at this point so making the display visible is OK
+
+    //console.log(newImg);
   }
   //console.log('choiceArr', choiceArr);
   //fading the images and clears them
